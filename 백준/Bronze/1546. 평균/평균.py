f18e02afda803044440206fbfb525c485611228f
KAWG NA YEONG <166089370+rhkrskdud@users.noreply.github.com>
2025-01-25 01:44:17 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+max = max(arr)
+sum = 0
+
+for i in range(n):
+    arr[i] = arr[i]/max*100
+    sum = sum + arr[i]
+    
+print(sum/n)
+
