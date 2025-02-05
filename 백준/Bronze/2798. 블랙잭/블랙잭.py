7896a6876be814b768eccc1fd5805bc617d04c26
KAWG NA YEONG <166089370+rhkrskdud@users.noreply.github.com>
2025-02-06 02:12:16 +0900

    [Bronze II] Title: 블랙잭, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -9,7 +9,7 @@ max_ = 0
 for i in range(n-2):
     for j in range(i+1, n-1):
         for k in range(j+1,n):
-            if max_ < arr[i] + arr[j] + arr[k] and arr[i] + arr[j] + arr[k] <= m:
+            if max_ <= arr[i] + arr[j] + arr[k] and arr[i] + arr[j] + arr[k] <= m:
                 max_ = arr[i] + arr[j] + arr[k]
 
 print(max_)
