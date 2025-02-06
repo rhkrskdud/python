2316194563df788894cc75302a04c24e83342685
KAWG NA YEONG <166089370+rhkrskdud@users.noreply.github.com>
2025-02-07 03:04:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 564 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -4,10 +4,11 @@ count = 0
 #666부터 시작
 num = 666
 
-#숫자 666을 문자화 해서 문자화한 num안에 666이라는 글자가 있는지 확인..
-while count < n:
-    if "666" in str(num):
+while(True):
+    #숫자 666을 문자화 해서 문자화한 num안에 666이라는 글자가 있는지 확인..
+    if '666' in str(num):
         count += 1
+        if count  == n:
+            print(num)
+            break
     num += 1
-
-print(num - 1)
