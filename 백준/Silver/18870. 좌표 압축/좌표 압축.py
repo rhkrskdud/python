d6e7facc6fc6b0aa0fb6daf036213ed33d38b9fc
KAWG NA YEONG <166089370+rhkrskdud@users.noreply.github.com>
2025-02-06 23:35:23 +0900

    [Silver II] Title: 좌표 압축, Time: 1644 ms, Memory: 159320 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -6,7 +6,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 arr1 = sorted(set(arr))
-
+#딕셔너리 방식은 한번 매핑해두면 값을 찾는데 걸리는 시간이 O(n) 이기 때문이다..
 compression = {v: i for i, v in enumerate(arr1)}
 
 result = [compression[x] for x in arr]
